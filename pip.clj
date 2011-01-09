795f0f4a8368ee5a46b9f31b0ebe1d6868b729e6
Ronald Kaiser <raios.catodicos@gmail.com>
2011-01-09 18:14:19 -0200

    pip optimized

---
+++ pip.clj
@@ -0,0 +1,99 @@
+; Implementation of Perceptually Important Points Algorithm
+; Author: Ronald Andreu Kaiser
+; Third Version - Optimized
+; Last Updated: Mon 29 Nov 2010 01:01:45 AM BRST
+
+
+(ns holmes.time-series.pip
+  (:require
+    [clojure.contrib.math :as math]))
+
+
+(defn vertical-distance
+  "Returns the vertical distance between three points.
+   P1 [x1 y1] and P2 [x2 y2] represent the extreme left and right PIPs respectively.
+   P3 [x3 y3] is the candidate."
+  [[x1 y1] [x3 y3] [x2 y2]]
+  (math/abs
+    (- (+ (double y1)
+      (* (- (double y2) (double y1))
+        (/ (- (double x3) (double x1))
+          (- (double x2) (double x1)))))
+      (double y3))))
+
+
+(defn max-in-interval
+  "Returns a vector with the best distance thorough the interval with the
+   corresponding index in the original-series.
+   Assumes that there is at least one point between P1 and P2."
+  [original-series [p1-index p2-index :as interval]]
+  (let [p1 (original-series p1-index)
+        p2 (original-series p2-index)]
+    (reduce
+      (fn [[last-best-distance last-best-index :as last-best] candidate-index]
+        (let [current-distance (vertical-distance p1 (original-series candidate-index) p2)]
+          (if (> (double current-distance) (double last-best-distance))
+            [current-distance candidate-index]
+            last-best)))
+      [-1 -1]
+      (lazy-seq (range (inc p1-index) p2-index)))))
+
+
+(defn find-max-from-intervals
+  "Returns the index of the point in original-series that is most
+   distant and the subjacent interval."
+  [original-series intervals]
+  (rest
+    (reduce
+      (fn [[last-best-distance last-best-index last-interval :as last-best] current-interval]
+        (let [current-best
+              (if (> (- (int (current-interval 1)) (int (current-interval 0))) 1)
+                (max-in-interval original-series current-interval)
+                last-best)]
+          (if (> (double (current-best 0)) (double last-best-distance))
+            [(current-best 0) (current-best 1) current-interval]
+            last-best)))
+      [-1 -1 [-1 -1]]
+      intervals)))
+
+
+(defn format-output
+  "Returns a vector, which is a transformation of the
+   current-intervals to points in the original-series
+   representing the final PIPs."
+  [original-series current-intervals]
+  (let [sorted-intervals (sort-by first (vec current-intervals))]
+    (reduce
+      (fn [last-interval current-interval]
+        (conj last-interval (original-series (current-interval 1))))
+      [(original-series (ffirst sorted-intervals))]
+      sorted-intervals)))
+
+
+(defn pip
+  "Returns a vector with the best k PIPs from original-series.
+   If k is greater or equal the length of original-series,
+   returns the same original-series."
+  [k original-series]
+  (binding [max-in-interval (memoize max-in-interval)]
+    (let [original-length (int (count original-series))
+          k (int k)]
+      (if (or (< k (int 3))
+        (< original-length (int 3))
+        (>= k original-length))
+        original-series
+        (let [current-intervals #{[0 (- original-length 1)]}]
+          (format-output
+            original-series
+            (nth
+              (iterate
+                (fn [current-intervals]
+                  (let [new-pip-interval
+                        (find-max-from-intervals original-series (lazy-seq current-intervals))
+                        point-to-split (first new-pip-interval)
+                        interval-to-split (second new-pip-interval)]
+                    (conj (disj current-intervals interval-to-split)
+                      [(first interval-to-split) point-to-split]
+                      [point-to-split (second interval-to-split)])))
+                current-intervals)
+              (int (- k 2)))))))))
---
+++ test_pip.clj
@@ -0,0 +1,141 @@
+(ns holmes.time-series.test-pip
+  (:use holmes.time-series.pip clojure.test)
+  (:require
+    [clojure.contrib.math :as math]
+    [clojure.contrib.generic.math-functions :as math-functions]))
+
+;==================================
+; find-max-from-intervals tests
+;==================================
+
+(deftest find-max-from-intervals-test
+  (let [input [[[1 2] [3 4] [5 30] [6 5] [9 7]] #{[0 3] [3 4]}]
+        expected [2 [0 3]]
+        result (apply find-max-from-intervals input)]
+    (is (= expected result)))
+  (let [input [[[1 2] [3 4] [5 30] [6 5] [9 7]] #{[0 4]}]
+        expected [2 [0 4]]
+        result (apply find-max-from-intervals input)]
+    (is (= expected result))))
+
+;==================================
+; max-in-interval tests
+;==================================
+
+(deftest max-in-interval-simple-case
+  (let [input [[[1 2] [3 4] [5 30] [9 7]] [0 3]]
+        expected [25.5 2]
+        result (apply max-in-interval input)]
+    (is (math-functions/approx= (first expected) (first result) 0.00001))))
+
+(deftest max-in-interval-simple-case-2
+  (let [input [[[1 2] [3 1] [5 30] [9 2]] [0 3]]
+        expected [28 2]
+        result (apply max-in-interval input)]
+    (is (math-functions/approx= (first expected) (first result) 0.00001))))
+
+;==================================
+; pip tests
+;==================================
+
+(deftest pip-empty-input
+  (let [input []
+        expected []
+        k 2
+        result (pip k input)]
+    (is (= expected result))))
+
+(deftest pip-one-observation-input
+  (let [input [[1 2]]
+        expected [[1 2]]
+        k 3
+        result (pip k input)]
+    (is (= expected result))))
+
+(deftest pip-two-observations-input
+  (let [input [[1 2] [2 3]]
+        expected [[1 2] [2 3]]
+        k 3
+        result (pip k input)]
+    (is (= expected result))))
+
+(deftest pip-simple-test-case
+  (let [input [[1 2] [2 3] [3 50] [4 10]]
+        expected [[1 2] [3 50] [4 10]]
+        k 3
+        result (pip k input)]
+    (is (= expected result))))
+
+(deftest pip-required-more-points-than-series-length
+  (let [input [[1 2] [2 3] [3 50] [4 10]]
+        expected input
+        k 10
+        result (pip k input)]
+    (is (= expected result))))
+
+(deftest pip-testing-if-pip-is-evaluating-intervals-correctly
+  (let [input [[0 1] [1 2] [2 4] [3 10] [4 20] [5 1] [6 3]
+               [7 1] [8 10] [9 1] [10 10] [11 5] [12 30] [13 5]
+               [14 1] [15 9] [16 13] [17 1] [18 0] [19 20] [20 1]
+               [21 2] [22 4] [23 10] [24 5] [25 2] [26 50] [27 0]
+               [28 7] [29 8] [30 5] [31 4] [32 3] [33 2] [34 20]
+               [35 1] [36 3] [37 1] [38 10] [39 1] [40 10] [41 5]
+               [42 30] [43 5] [44 1] [45 9] [46 13] [47 1] [48 0]
+               [49 20] [50 1] [51 2] [52 4] [53 10] [54 20] [55 1]
+               [56 3] [57 1] [58 10] [59 1] [60 10] [61 5] [62 30]
+               [63 5] [64 1] [65 9] [66 13] [67 1] [68 0] [69 20]
+               [70 1] [71 2] [72 4] [73 10] [74 5] [75 2] [76 50]
+               [77 0] [78 7] [79 8] [80 5] [81 4] [82 3] [83 2]
+               [84 20] [85 1] [86 3] [87 1] [88 10] [89 1] [90 10]
+               [91 5] [92 30] [93 5] [94 1] [95 9] [96 13] [97 1]
+               [98 0] [99 20]]
+        expected [[0 1] [12 30] [25 2] [26 50] [27 0]
+                  [42 30] [75 2] [76 50] [77 0] [99 20]]
+        k 10
+        result (pip k input)]
+    (is (= expected result))))
+
+;==================================
+; vertical-distance tests
+;==================================
+
+(deftest vertical-distance-colinearity
+  (let [p1 [1, 2]
+        p3 [2, 3]
+        p2 [3, 4]
+        expected 0
+        result (vertical-distance p1 p3 p2)]
+    (is (math-functions/approx= expected result 0.00001))))
+
+(deftest vertical-distance-positive
+  (let [p1 [1, 2]
+        p3 [2, 5]
+        p2 [3, 2]
+        expected 3
+        result (vertical-distance p1 p3 p2)]
+    (is (math-functions/approx= expected result 0.00001))))
+
+(deftest vertical-distance-negative
+  (let [p1 [1, 2]
+        p3 [2, -1]
+        p2 [3, 2]
+        expected 3
+        result (vertical-distance p1 p3 p2)]
+    (is (math-functions/approx= expected result 0.00001))))
+
+(deftest vertical-distance-float-positive
+  (let [p1 [1, 2]
+        p3 [2, 4.8]
+        p2 [3, 5]
+        expected 1.3
+        result (vertical-distance p1 p3 p2)]
+    (is (math-functions/approx= expected result 0.00001))))
+
+(deftest vertical-distance-float-negative
+  (let [p1 [1, 2]
+        p3 [2, -10.0]
+        p2 [3, 5]
+        expected 13.5
+        result (vertical-distance p1 p3 p2)]
+    (is (math-functions/approx= expected result 0.00001))))
+
